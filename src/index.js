fb702ef1a0060cd81b40339da27c40fa9d352d91
David Mason <drdmason@gmail.com>
2015-10-13 11:19:06 +1000

    fix(exports): use standard aggregation module export pattern
    Was using a form of exports that did not play well with standard es2015
    import syntax.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
-'use strict'
-
-import Select from './components/Select/lib/Select'
-import Button from './components/Button'
-import Input from './components/Input'
-import Icons from './components/Icons'
-import Icon from './components/Icon'
-import Loader from './components/Loader'
-import Overlay from './components/Overlay'
-import OverlayTrigger from './components/OverlayTrigger'
-import Tooltip from './components/Tooltip'
-import Modal from './components/Modal'
-import FixedDataTable from './components/FixedDataTable'
-import KeyComponent from './components/KeyComponent'
-
-const Components = {
-  Select: Select,
-  Button: Button,
-  Input: Input,
-  Icons: Icons,
-  Icon: Icon,
-  Loader: Loader,
-  Overlay: Overlay,
-  OverlayTrigger: OverlayTrigger,
-  Tooltip: Tooltip,
-  Modal: Modal,
-  FixedDataTable: FixedDataTable,
-  KeyComponent: KeyComponent
-}
-
-export default Components
+export Select from './components/Select/lib/Select'
+export Button from './components/Button'
+export Input from './components/Input'
+export Icons from './components/Icons'
+export Icon from './components/Icon'
+export Loader from './components/Loader'
+export Overlay from './components/Overlay'
+export OverlayTrigger from './components/OverlayTrigger'
+export Tooltip from './components/Tooltip'
+export Modal from './components/Modal'
+export FixedDataTable from './components/FixedDataTable'
+export KeyComponent from './components/KeyComponent'
